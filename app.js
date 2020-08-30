1953a268a515fcc1ad2dc193a2b1631fc8fcdc9f
JK <jas.kocurek@wp.pl>
2020-08-30 12:56:00 +0200

    app building init

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose');
+
+const app = express()
+const port = 3000
+
+
+dbURI = 'mongodb+srv://admin:root@cluster0.gqhx8.mongodb.net/shopping-list?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }).then(result => {
+    console.log('DB connection established!');
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+}).catch(err => {
+    console.log(err);
+});
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+
+app.use((req, res) => {
+    res.status(404).render("404")
+})
