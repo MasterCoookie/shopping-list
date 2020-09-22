912b22bee017ca293ee30b5fd760b565639ff433
JK <jas.kocurek@wp.pl>
2020-09-22 13:25:07 +0200

    list protection working!

--- middleware/listMiddleware.js
+++ middleware/listMiddleware.js
@@ -2,14 +2,36 @@ const List = require('../models/list');
 const objectId = require('mongodb').ObjectId;
 
 const checkViewPermit = (req, res, next) => {
-    console.log(req.params);
     if(req.params.id) {
         const userId = res.locals.user._id;
         const listId = objectId(req.params.id);
 
-        console.log(listId);
+        List.findById(listId).then(result => {
+            // if user is author than go on
+            if(userId == result.authorId.toString()) {
+                next();
+            } else {
+                // if user isnt author and the list was never shared deny
+                if(!result.sharedWith) {
+                    console.log("ACCESS DENIED!");
+                    res.redirect('/mylists');
+                // if list was shared check who with
+                } else {
+                    if(result.sharedWith.includes(userId)) {
+                        next();
+                    } else {
+                        console.log("ACCESS DENIED!");
+                        res.redirect('/mylists');
+                    }
+                }
+            }
+        }).catch(err => {
+            console.log(err);
+            res.redirect('/mylists')
+        })
+    } else {
+        next();
     }
-    next();
 }
 
 module.exports = { checkViewPermit };
--- routes/listsRoutes.js
+++ routes/listsRoutes.js
@@ -4,12 +4,13 @@ const listMiddleware = require('../middleware/listMiddleware');
 
 const router = express.Router();
 
+
+router.get('/list/create', listsController.create_get);
+router.post('/list/create', listsController.create_post);
 router.get('/list/:id', listMiddleware.checkViewPermit, listsController.list_get);
 router.put('/list/:id', listMiddleware.checkViewPermit, listsController.list_put);
 router.delete('/list/:id', listMiddleware.checkViewPermit, listsController.list_delete);
 router.get('/', listsController.lists_get);
-router.get('/list/create', listsController.create_get);
-router.post('/list/create', listsController.create_post);
 router.get('/share/:id', listsController.share);
 router.get('/shared/', listsController.shared_get);
 
