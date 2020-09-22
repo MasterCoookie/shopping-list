8ceb99d96b17a84a8718d1e3330e377ca123876d
JK <jas.kocurek@wp.pl>
2020-09-22 12:56:18 +0200

    stage fixed

--- routes/listsRoutes.js
+++ routes/listsRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const listsController = require('../controllers/listsController');
+const listMiddleware = require('../middleware/listMiddleware');
 
 const router = express.Router();
 
-router.get('/list/:id', listsController.list_get);
-router.put('/list/:id', listsController.list_put);
-router.delete('/list/:id', listsController.list_delete);
+router.get('/list/:id', listMiddleware.checkViewPermit, listsController.list_get);
+router.put('/list/:id', listMiddleware.checkViewPermit, listsController.list_put);
+router.delete('/list/:id', listMiddleware.checkViewPermit, listsController.list_delete);
 router.get('/', listsController.lists_get);
-router.get('/create', listsController.create_get);
-router.post('/create', listsController.create_post);
+router.get('/list/create', listsController.create_get);
+router.post('/list/create', listsController.create_post);
 router.get('/share/:id', listsController.share);
 router.get('/shared/', listsController.shared_get);
 
